1cc8766a7252725d1de817cec287cf6dc2ea45a1
Noble Ackerson <noble.ackerson@gmail.com>
2023-03-10 13:37:58 -0500

    chore: post-demo cleanup

--- utilities.py
+++ utilities.py
@@ -1,17 +1,6 @@
 import openai
-import config
 
 class GPT_utilities:
     
     def __init__(self):
-        openai.api_key = config.openai_api_key
-        
-        self.char_to_token = 4 # approximatly four characters is 1 token
-        self.default_gpt_config = {
-            "model": "text-davinci-003",
-            "temperature": 0.7,
-            "max_length":200,
-            "frequency_penalty": 0,
-            "presence_penalty": 0
-        }
-        self.gpt_config = self.default_gpt_config.copy()
+        openai.api_key = "sk-"
